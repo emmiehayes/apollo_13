6564821c1ea6cc2cca93e22bdef9d1a7d4e1c71c
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-06 10:40:17 -0600

    add spec folder, feature folder and paste in user stories

---
+++ spec/features/user_sees_astronaut_index_spec.rb
@@ -0,0 +1,34 @@
+=begin
+```
+As a visitor,
+When I visit '/astronauts'
+I see a list of astronauts with the following info:
+ - Name
+ - Age
+ - Job
+ (e.g. "Name: Neil Armstrong" Age: 37 Job: Commander")
+```
+
+```
+As a visitor,
+When I visit '/astronauts'
+I see the average age of all astronauts.
+(e.g. "Average Age: 34")
+```
+
+```
+As a visitor,
+When I visit '/astronauts'
+I see a list of the space missions' in alphabetical order for each astronaut.
+(e.g "Apollo 13"
+     "Capricorn 4"
+     "Gemini 7")
+```
+
+```
+As a visitor,
+When I visit '/astronauts'
+I see the total time in space for each astronaut.
+(e.g. "Total Time in Space: 760 days")
+```
+=end
