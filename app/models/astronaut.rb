a3d7dcfe208defc4d8dd5858b80584b9c504e528
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-06 11:35:29 -0600

    add astronaunt space mission model, add relationships

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,7 +1,8 @@
 class Astronaut < ApplicationRecord
   validates_presence_of :name, :age, :job
+  has_many :space_missions
 
-  def self.average_age 
+  def self.average_age
     average(:age)
   end
 end
