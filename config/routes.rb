6ede2bfd9f745ac0699af35c7300bf19e7506e35
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-06 11:34:47 -0600

    add resources to routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :astronauts
+  resources :space_missions
+  resources :astronaut_space_missions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
